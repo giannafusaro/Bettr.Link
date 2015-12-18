b71d66a4b8f16d873986f7ce0273a0c6a999e679
Jeff Miller <jeff@humani.se>
2015-12-18 02:56:26 -0800

    Add meta scraping to analyzer w/ MetaInspector gem
    * Pass HTML directly to prevent additional requests
    * Normalize URL w/ MetaInspector::URL
    * Scrape "best" title, images, desc, feed, metas

--- app/services/analyzer.rb
+++ app/services/analyzer.rb
@@ -5,10 +5,13 @@ class Analyzer
   include Capybara::DSL
   include ActiveSupport::Configurable
 
-  attr_accessor :uri, :technologies
+  attr_reader :url, :technologies, :content, :to_hash
+  alias :html :content
 
   def initialize(url)
-    @uri = URI url
+    # Remove any known trackers
+    @url = MetaInspector::URL.new(url)
+    @url.untrack!
 
     # Import App and Category lists
     @maps = JSON.parse File.read(config.apps)
@@ -20,26 +23,42 @@ class Analyzer
   end
 
   def process!
-    @session.visit @uri
-    @technologies = run_wappalyzer
-    @session.driver.quit
+    @session.visit @url.url
+    @headers = @session.response_headers
+    @content = @session.html.encode('UTF-8', invalid: :replace, undef: :replace)
 
-    { technologies: @technologies, keywords: [] }
+    @metadata = run_metainspector
+    @technologies = run_wappalyzer
+    #keywords = run_keywords
+    #whois = run_whois
+
+    @session.driver.quit
+    @to_hash = @metadata.merge({ technologies: @technologies, keywords: [] })
   end
 
   private
 
+    def run_metainspector
+      page = MetaInspector::Document.new @url.url, document: @content
+      {
+        title: page.best_title,
+        description: page.description,
+        image: page.images.best,
+        feed: page.feed,
+        meta_tags: page.meta
+      }
+    end
+
     def run_wappalyzer
-      # Parse header and content data (header keys must be lowercase)
-      @headers = @session.response_headers.each_with_object({}){ |s,h| h[s[0].downcase] = s[1] }
-      @content = @session.html.encode('UTF-8', invalid: :replace, undef: :replace)
-      @data = { host: @uri.host, url: @uri.to_s, headers: @headers, html: @content }
+      # Parse header data (keys must be lowercase) and prep driver.data
+      headers = @headers.each_with_object({}){ |(k,v),h| h[k.downcase] = v }
+      data = { host: @url.host, url: @url.url, headers: headers, html: @content }
 
       # Pass data to Wappalyzer scripts
       @session.execute_script %Q(
         w.apps = #{@apps.to_json};
         w.categories = #{@categories.to_json};
-        w.driver.data = #{@data.to_json};)
+        w.driver.data = #{data.to_json};)
 
       # Perform analysis and end session
       JSON.parse @session.evaluate_script "w.driver.init();"
